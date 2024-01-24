a44059199f3ec262c33bd7b0965b3c16cbedb761
Bryan Bradley <bryrbradley@gmail.com>
2024-01-23 19:14:17 -0700

    added Objective2Lab2.java and .class

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,15 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Bryan Bradley";
+    int age = 37;
+    String state = "Rio Piedras, Puerto Rico";
+    System.out.print("Name:");
+    System.out.print(name\t);
+    System.out.print("Age:"); 
+    System.out.print(age\t);
+    System.out.print("State:");
+    System.out.print(state);
+
+    
+  }
+}
