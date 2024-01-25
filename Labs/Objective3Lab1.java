c9611782ad037fc911df184d5ba5b929b865a2f8
Bryan Bradley <bryrbradley@gmail.com>
2024-01-25 14:29:13 -0700

    added Ojective3Lab1. jav and class

--- Labs/Objective3Lab1.java
+++ Labs/Objective3Lab1.java
@@ -1,9 +1,10 @@
 public class Objective3Lab1 {
   public static void main(String[] args) {
-  int num1 = 15;
-  int num2 = 6;
-  int num3 = 2; 
-  double average = ((num1 * 2 + num2 + num3)/3);
-  System.out.println();
+  double num1 = 2;
+  double num2 = 6;
+  double num3 = 15; 
+  num1 = num1 * 2; 
+  double average = (num1 + num2 + num3)/3;
+  System.out.println(average);
   }
 }
