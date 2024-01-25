1becb607eda184d638a3bbee33618413127236a8
Bryan Bradley <bryrbradley@gmail.com>
2024-01-25 13:09:48 -0700

    Added Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+  int year = 1986;
+  String month_Born = "August";
+  var weekend = false;
+  System.out.println(year);
+  System.out.println(month_Born);
+  System.out.println(weekend);
+
+  }
+}
