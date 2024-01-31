0f6942f0945a81043e9d3419ef9e9de8d3694821
Bryan Bradley <bryrbradley@gmail.com>
2024-01-31 15:57:16 -0700

    Added Objective8Lab1.java and .class

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,18 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1 to 10
+
+    //TODO Write another for loop that will print the numbers 10 to 1.
+
+     
+  
+     for (int j = 1; j<=10; j++){
+    
+    System.out.println(j);
+    }
+     for (int i = 10; i>=1; i--){
+    
+    System.out.println(i);
+    }
+  }
+}
